f100b32c09264c5d847e84d3bba0ca1c98ff030d
kero1976 <kero1976@outlook.jp>
2023-05-28 10:22:44 +0900

    blackの説明を追加

--- Python/AWSサンプル/src/kero/S3Sample.py
+++ Python/AWSサンプル/src/kero/S3Sample.py
@@ -1,9 +1,21 @@
-'''
+"""
 S3にファイルがあるかどうかを確認する
-'''
+"""
 
 import boto3
 
-print("ハロー世界")
+# S3への接続
+s3 = boto3.client("s3")
 
-print('Hellow')
+# バケット名
+bucket_name = "u10.jp"
+
+# バケット内のオブジェクトのリストを取得
+response = s3.list_objects_v2(Bucket=bucket_name)
+
+# オブジェクトのリストを表示
+if "Contents" in response:
+    for obj in response["Contents"]:
+        print(obj["Key"])
+else:
+    print("バケット内にオブジェクトはありません。")
