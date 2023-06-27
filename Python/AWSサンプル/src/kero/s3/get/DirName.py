4741f29954eca97e433f18051c71dd8f1bca2c28
kero1976 <kero1976@outlook.jp>
2023-06-27 10:57:33 +0900

    フォルダ一覧取得を作成

---
+++ Python/AWSサンプル/src/kero/s3/get/DirName.py
@@ -0,0 +1,88 @@
+"""
+S3にファイルがあるかどうかを確認する
+"""
+import os
+import traceback
+from logging import config, getLogger
+
+import boto3
+import botocore
+from termcolor import colored, cprint
+
+config.fileConfig(os.path.join(os.path.dirname(__file__) , "../../logging.ini"), encoding='utf-8')
+logger = getLogger(__name__)
+
+
+
+def s3list(s3, bucket_name):
+    logger.info({"action": 'start',
+                'param': {
+                    's3': s3,
+                    'bucket_name': bucket_name
+                }})
+    list = []
+    if not bucket_name:
+        msg = {
+            "action": "fail",
+            "msg": "Parameter is None.",
+        }
+        logger.debug(msg)
+        logger.error("Bucket name not specified.")
+        return
+    # バケット内のオブジェクトのリストを取得
+    try:
+        response = s3.list_objects_v2(Bucket=bucket_name, Delimiter='/')
+    except botocore.exceptions.ClientError as err:
+        msg = {
+            "action": "fail",
+            "msg": "list_objects_v2 err",
+            "exception": err,
+            "type": type(err),
+            "traceback": traceback.format_exception(err)
+        }
+        code = err.response["Error"]["Code"]
+        logger.error(f"S3 Request Error.({code}):Bucketname={bucket_name}")
+        logger.debug(msg)
+        return
+    except botocore.exceptions.NoCredentialsError as err:
+        msg = {
+            "action": "fail",
+            "msg": "list_objects_v2 err",
+            "exception": err,
+            "type": type(err)
+        }
+        logger.error(f"NoCredentialsError.")
+        logger.debug(msg)
+        return
+    except Exception as err:
+        msg = {
+            "action": "fail",
+            "msg": "list_objects_v2 err",
+            "exception": err,
+            "type": type(err),
+            "traceback": traceback.format_exception(err)
+        }
+        logger.debug(msg)
+        return
+
+    # オブジェクトのリストを表示
+    if "CommonPrefixes" in response:
+        for obj in response["CommonPrefixes"]:
+            list.append(obj["Prefix"])
+    else:
+        print("バケット内にオブジェクトはありません。")
+    logger.info({
+        'action': 'success',
+        'return': list,
+        "cnt": len(list)
+    })
+    return list
+
+
+def s3listInput():
+    logger.debug({"action": 'start'})
+    buketname = input(colored("[+]Bucket Name:", "green"))
+    s3list(boto3.client("s3"), buketname)
+
+if __name__ == '__main__':
+    s3listInput()
