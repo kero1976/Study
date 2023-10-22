1e994e5cf80d3e34fd270afb2ce62a1eecc7d33d
kero1976 <kero1976@outlook.jp>
2023-10-22 11:18:15 +0900

    更新

---
+++ AWSCLI/Cloudformation/DynamoDB.md
@@ -0,0 +1,11 @@
+# Type
+
+## AWS::DynamoDB::Table
+
+## AWS::DynamoDB::GlobalTable
+
+## 違い
+Globalにした方が良さそう。
+
+# ContributorInsightsSpecification
+ClpudWatch寄稿者インサイトを有効または無効にするために使用する設定
--- Java/JavaSample1/Sample1.java
+++ Java/JavaSample1/Sample1.java
@@ -1,5 +1,20 @@
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Collections;
+
 public class Sample1{
     public static void main(String[] args){
-        System.out.println("ABC");
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("A1", "a3");
+        map.put("A3", "a2");
+        map.put("A2", "a4");
+        map.put("B1", "b3");
+        map.put("B3", "b2");
+        map.put("B2", "b4");
+        List<String> list = map.entrySet().stream().sorted(Map.Entry.comparingByValue()).map(e -> e.getKey()).collect(Collectors.toList());
+        System.out.println(list);
     }
 }
