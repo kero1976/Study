848d8c7b4b7b288a9d28a8d93d6779b69dfd4908
kero1976 <kero1976@outlook.jp>
2024-03-24 20:01:54 +0900

    add

--- Python/AWSサンプル/dynamoutil/dynamoutil/localfile/file.py
+++ Python/AWSサンプル/dynamoutil/dynamoutil/localfile/file.py
@@ -1,4 +1,11 @@
-def create_file(file_name: str, data, dir: str=None) -> bool:
+from logging import getLogger
+import os
+import datetime
+
+
+logger = getLogger(__name__)
+
+def create_file(file_name: str, datas: list[dict], dir: str=None) -> bool:
     """ファイルを作成する
 
     Args:
@@ -9,4 +16,14 @@ def create_file(file_name: str, data, dir: str=None) -> bool:
     Returns:
         bool: True:ファイル作成成功、False:ファイル作成失敗
     """
+    logger.debug(f"create_file start(file_name={file_name}, dir={dir})")
+    if dir:
+        path = f"{dir}/{file_name}"
+    else:
+        path = f"./{file_name}"
+    logger.debug(f"path={os.path.abspath(path)}")
+    
+    with open(path, mode="w") as f:
+        for data in datas:
+            f.write(data)
     return True
--- Python/AWSサンプル/dynamoutil/tests/localfile/test_file.py
+++ Python/AWSサンプル/dynamoutil/tests/localfile/test_file.py
@@ -1,4 +1,4 @@
 from dynamoutil.localfile.file import create_file
 
-def test_create_file():
-    assert create_file(None, None) == True
+def test_create_file(tmpdir):
+    assert create_file("data1", ["a", "b"], tmpdir) == True
