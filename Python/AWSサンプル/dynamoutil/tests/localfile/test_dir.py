31d3c73d57cd47807da6ce2db99f959d18df067c
kero1976 <kero1976@outlook.jp>
2024-03-24 14:01:29 +0900

    テストフォルダを指定

--- Python/AWSサンプル/dynamoutil/tests/localfile/test_dir.py
+++ Python/AWSサンプル/dynamoutil/tests/localfile/test_dir.py
@@ -2,20 +2,27 @@ from dynamoutil.localfile.dir import create_dir, create_date_dir
 import os
 import pytest
 
+
 def test_create_dir_ok1(tmpdir):
+    """一時フォルダに移動して、カレントフォルダにフォルダを作成。成功"""
+    os.chdir(tmpdir)
     assert create_dir("xyz") == True
 
-@pytest.mark.shijo
 def test_create_dir_ok2(tmpdir):
+    """指定した一時フォルダにフォルダを作成。成功"""
     assert create_dir("xyz", tmpdir) == True
     
-def test_create_dir_ng1():
-    assert create_dir("C:/abc", "xyz") == False
-    assert create_dir("abc", "C:/abc") == False
+def test_create_dir_ng1(tmpdir):
+    """一時フォルダに移動して、カレントフォルダにフォルダを作成。失敗"""
+    os.chdir(tmpdir)
+    assert create_dir("C:/abc") == False
+    with open("xyz","w"):pass
+    assert create_dir("xyz") == False
 
-def test_create_dir_ng2():
-    assert create_dir("C:/abc", "xyz") == False
-    assert create_dir("abc", "C:/abc") == False
+def test_create_dir_ng2(tmpdir):
+    assert create_dir("C:/abc", tmpdir) == False
+    with open(f"{tmpdir}/xyz","w"):pass
+    assert create_dir("xyz", tmpdir) == False
       
 def test_create_date_dir():
     assert create_date_dir("C:/abc") == True
