b3b3c2e0d0dd7361e23c7be07b2a5729613245b1
kero1976 <kero1976@outlook.jp>
2024-04-24 21:06:43 +0900

    add

---
+++ セキュリティ/証明書/三層階層証明書作成.bat
@@ -0,0 +1,89 @@
+@echo off
+
+set BASE=c:\work\openssl
+
+echo 作業フォルダを作成します。
+mkdir %BASE%\self_ca
+
+mkdir %BASE%\self_ca\RCA
+mkdir %BASE%\self_ca\ICA
+mkdir %BASE%\self_ca\server
+
+echo ================================================================
+echo 1.Create Root CA
+echo ================================================================
+cd %BASE%\self_ca\RCA
+
+echo 1-1.Create Root CA Directory
+mkdir demoCA
+mkdir demoCA\certs
+mkdir demoCA\crl
+mkdir demoCA\newcerts
+mkdir demoCA\private
+type nul > demoCA\index.txt
+
+echo 1-2.Create Root CA request
+
+rem -nodesでパスワードなし。
+openssl req -new -nodes -subj /CN=test.root.com/OU=B/O=A/ST=Tokyo/C=JP -keyout demoCA/private/cakey.pem -out demoCA/careq.pem
+
+
+echo 1-3.Root CA request Sign
+openssl ca -create_serial -out demoCA\cacert.pem -days 1095 -batch -keyfile demoCA/private/cakey.pem -selfsign -extensions v3_ca -infiles demoCA/careq.pem 
+copy demoCA\cacert.pem demoCA\cacert.pem.cer
+
+echo ================================================================
+echo 2.Create CA
+echo ================================================================
+cd %BASE%\self_ca\ICA
+
+
+echo 2-1.Create CA request
+openssl req -new -nodes -subj /CN=test.ca.jp/OU=B/O=A/ST=Tokyo/C=JP -keyout newkey.pem -out newreq.pem -days 1095
+
+echo 2-2.CA Sign
+cd %BASE%\self_ca\RCA
+rem -batchでサインするかの確認をスキップする
+openssl ca -batch -policy policy_anything -out ../ICA/newcert.pem -extensions v3_ca -infiles ../ICA/newreq.pem 
+
+
+echo 2-3.Create CA Directory
+cd %BASE%\self_ca\ICA
+mkdir demoCA
+mkdir demoCA\certs
+mkdir demoCA\crl
+mkdir demoCA\newcerts
+mkdir demoCA\private
+type nul >  demoCA\index.txt
+move newkey.pem demoCA\private\cakey.pem
+move newcert.pem demoCA\cacert.pem
+copy demoCA\cacert.pem demoCA\cacert.pem.cer
+
+openssl x509 -in demoCA/cacert.pem -noout -next_serial -out demoCA/serial
+
+echo ================================================================
+echo 3.Create Chain
+echo ================================================================
+cd %BASE%\self_ca
+openssl x509 -outform pem -in RCA/demoCA/cacert.pem -out rca.pem
+openssl x509 -outform pem -in ICA/demoCA/cacert.pem -out ica.pem
+openssl verify -CAfile rca.pem ica.pem 
+copy /b rca.pem + ica.pem  cert_chain.pem
+
+echo ================================================================
+echo 4.Create WEB Server
+echo ================================================================
+
+echo 4-1.Create WEB private key
+cd %BASE%\self_ca\server
+openssl genrsa 2048 > server_key.pem
+
+echo 4-2.Create WEB request
+openssl req -new -subj /CN=test.web.jp/OU=B/O=A/ST=Tokyo/C=JP -key server_key.pem -out server_req.pem
+
+echo 4-3.Create WEB sign
+cd %BASE%\self_ca\ICA
+openssl ca -batch -in ../server/server_req.pem -keyfile demoCA/private/cakey.pem -cert demoCA/cacert.pem -out ../server/www_vubuntults_mydomain.crt -days 1095
+
+
+pause
